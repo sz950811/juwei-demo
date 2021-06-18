f03a04838de0c25d1069d70c5afd1a1e444f844e
沈政 <sz950811@outlook.com>
2021-06-18 18:04:47 +0800

    第六次修改

--- juwei/js/nav.js
+++ juwei/js/nav.js
@@ -5,3 +5,17 @@
 			$(this).children("ul").stop().slideToggle();
 		});
 	})
+	// 主题动画效果
+	$("#AnimatedPictures").hover(function () {
+	            $("#AnimatedPictures").animation({
+	                // 注意: margin-top也可写成驼峰式marginTop
+	                "margin-top": "400px",
+	                "opacity": "1.0",
+	                "width": "300px",
+	                "height": "300px",
+	                "left": "100px",
+					"background-color" : "blank",
+	            }, 3000, function () {
+	            });
+	        });
+	

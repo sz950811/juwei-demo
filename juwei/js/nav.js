bd19b1b07b0188c17f0b922effc6d621a80fda5b
沈政1 <sz950811@outlook.com>
2021-06-19 10:14:32 +0800

    第七次修改

--- juwei/js/nav.js
+++ juwei/js/nav.js
@@ -1,21 +1,7 @@
-
 	// 涓嬫媺鑿滃崟
 	$(function(){
 		$(".nav-3 > li").hover(function(){
 			$(this).children("ul").stop().slideToggle();
 		});
 	})
-	// 主题动画效果
-	$("#AnimatedPictures").hover(function () {
-	            $("#AnimatedPictures").animation({
-	                // 注意: margin-top也可写成驼峰式marginTop
-	                "margin-top": "400px",
-	                "opacity": "1.0",
-	                "width": "300px",
-	                "height": "300px",
-	                "left": "100px",
-					"background-color" : "blank",
-	            }, 3000, function () {
-	            });
-	        });
-	
+
